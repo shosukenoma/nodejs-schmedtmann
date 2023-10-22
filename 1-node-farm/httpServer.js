6f3f14d551370b0c9aa3c73699507b30eed05fc9
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-10-22 18:05:37 -0500

    read file and create server

---
+++ 1-node-farm/httpServer.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const url = require('url')
+const server = http.createServer((req, res) => {
+  const pathName = req.url
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the overview page.')
+  } else if (pathName === '/product') {
+    res.end('This is the product page.')
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    })
+    res.end('<h1>Page not found!</h1>')
+  }
+})
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000...');
+})
