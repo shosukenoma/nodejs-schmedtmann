e15a764ff5bd21dbc72a38881c696f59728ff3cc
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-10-23 10:22:39 -0500

    Add API request

--- 1-node-farm/httpServer.js
+++ 1-node-farm/httpServer.js
@@ -6,6 +6,8 @@ const server = http.createServer((req, res) => {
     res.end('This is the overview page.')
   } else if (pathName === '/product') {
     res.end('This is the product page.')
+  } else if (pathName === '/api') {
+    res.end('API')
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
